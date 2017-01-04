d70e191b48f8979a3619df97d48a089fba7b3d28
James Boullion <jboullion83@gmail.com>
2017-01-03 22:35:20 -0600

    Tic Tac Toe Nearly Done
    Nearly all of the functionality and AI are in place. The AI is currently
    unbeatable (afaik). Still needs some UX love for showing turns and maybe
    setting up 2 player

--- tictactoe/controller.js
+++ tictactoe/controller.js
@@ -1,17 +1,25 @@
 
-jQuery(document).ready(function($) {
+$(function() {
 
 	// Is it players turn?
 	var pTurn = true,
+		PLAYERS = 1,
+		currentTurn = 1,
 	//array to hold piece placements
 		pieceArray = [[0,0,0],[0,0,0],[0,0,0]],
 	//I don't know if this is stupid or genius
 	//Setting up values to use math to find winner
 		X_VALUE = 1,
-		Y_VALUE = 4,
+		O_VALUE = 4,
 		X_WIN = X_VALUE * 3,
-		Y_WIN = Y_VALUE * 3,
-		WINNER = '';
+		O_WIN = O_VALUE * 3,
+		X_RISK = X_WIN - X_VALUE,
+		O_ADV = O_WIN - O_VALUE,
+		WINNER = false,
+		DEBUG = true;
+
+	//we have an svg template we can use as our blank piece HTML
+	var pieceTemplate = $('#piece-tempate').html();
 
 	//Create our SVG Pieces :)
 	var oPath = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
@@ -32,85 +40,265 @@ jQuery(document).ready(function($) {
 	xPath2.setAttribute('x2', '20%');
 	xPath2.setAttribute('y2', '80%');
 
+	//used every round
+	var rowSum = 0,
+		colSum = 0,
+		ltrDiagSum = 0,
+		rtlDiagSum = 0,
+		rRow = 0;
 
 	/**
-	 * Place a piece on click
+	 * Place a piece on click. Even after new pieces are placed
 	 */
-	$('.piece.open').click(function(e){
+	$('#tictactoe-board').on('click', '.piece.open', function(e){
 		var svg = $(this)[0],
 			row = $(this).data('row'),
-			column = $(this).data('column');
+			col = $(this).data('column');
 
-		if(pTurn){
-			//Display X
-			var whatPath = xPath1.cloneNode(true);
-			svg.appendChild(whatPath);
-			whatPath = xPath2.cloneNode(true);
-			svg.appendChild(whatPath);
-
-			$(this).addClass('piece-x');
-			pieceArray[row][column] = X_VALUE;
-		}else{
-			//Display O
-			var whatPath = oPath.cloneNode(true);
-			svg.appendChild(whatPath);
-
-			$(this).addClass('piece-o');
-			pieceArray[row][column] = Y_VALUE;
-		}
-
-		$(this).removeClass('open');
+		displayX(row,col);
 
 		endTurn();
 	});
 
+	//Display the X svg image
+	function displayX(row, col){
+		var $svg = $('#square-'+row+'-'+col+' svg'),
+			svg = $svg[0];
+
+		$svg.removeClass('open').addClass('piece-x');
+		pieceArray[row][col] = X_VALUE;
+
+		var whatPath = xPath1.cloneNode(true);
+		svg.appendChild(whatPath);
+		whatPath = xPath2.cloneNode(true);
+		svg.appendChild(whatPath);
+	}
+
+	//Display the O svg image
+	function displayO(row, col){
+		var $svg = $('#square-'+row+'-'+col+' svg'),
+			svg = $svg[0];
+		console.log('row: '+row+', col: '+col);
+
+		$svg.removeClass('open').addClass('piece-o');
+		pieceArray[row][col] = O_VALUE;
+
+		var whatPath = oPath.cloneNode(true);
+		svg.appendChild(whatPath);
+	}
 
 	/**
 	 *  Ends Turn
 	 */
 	function endTurn(){
-		var rowSum = 0,
-			colSum = 0,
-			ltrDiagSum = 0,
-			rtlDiagSum = 0,
-			rRow = 0;
 
-		//are any of the rows in a win condition?
-		for(var row = 0; row < pieceArray.length; row++){
-			rowSum = pieceArray[row].reduce(add, 0);
-			colSum = 0;
-			//now check the nth (row) column
-			for(var col = 0; col < pieceArray[row].length; col++){
-				//reverse our cols and rows so we can check both directions
-				colSum += pieceArray[col][row];
-			}
-
-			ltrDiagSum += pieceArray[row][row];
-
-			//get the other diagonal
-			rRow = pieceArray.length - row - 1;
-			rtlDiagSum += pieceArray[rRow][row];
-
-			//Do we have a winner?
-			if(rowSum === X_WIN || colSum === X_WIN || ltrDiagSum === X_WIN || rtlDiagSum === X_WIN){
-				WINNER = 'X';
-			}else if (rowSum === Y_WIN || colSum === Y_WIN || ltrDiagSum === Y_WIN || rtlDiagSum === Y_WIN) {
-				WINNER = 'O';
-			}
-
-		}
+		WINNER = checkWin();
 
 		//Winner!
-		if(WINNER !== ''){
-			alert('You Win');
+		if(WINNER !== false){
+			alert(WINNER+' Wins!');
+			resetGame();
+			return;
+		}
+
+		//Did we run out of turns?
+		if(currentTurn === 9){
+			alert('DRAW!!');
+			resetGame();
+			return;
 		}
 
 		//No Winner this round, next turn;
 		pTurn = !pTurn;
+		currentTurn++;
+
+		//take next turn
+		if(PLAYERS === 1 && !pTurn){
+			aiTurn();
+			endTurn();
+		}
+
 	}
 
+	function aiTurn(){
+		ltrDiagSum = 0;
+		rtlDiagSum = 0;
+
+		if(DEBUG === true){
+			console.log('AI Turn');
+		}
+
+		//If AI goes first, then I think a corner is better
+		/*
+		if(pieceArray[0][0] === 0){
+			displayO(0,0);
+			endTurn();
+			return;
+		}
+		*/
+
+		//always take middle piece if possible.
+		if(pieceArray[1][1] === 0){
+			displayO(1,1);
+			return;
+		}
+
+		//are any of the rows in a win condition?
+		for(var row = 0; row < pieceArray.length; row++){
+			countPieces(row);
+
+			//Check our columns for adv and risk
+			if (advOrRisk(colSum)) {
+				//this column is in trouble, find an open space!
+				for(var col = 0; col < pieceArray[row].length; col++){
+					if(pieceArray[col][row] === 0){
+						//Open!
+						if(DEBUG === true){
+							console.log('column placement');
+						}
+						displayO(col,row);
+						return;
+					}
+				}
+			}
+
+			//Check our rows for adv and risk
+			if (advOrRisk(rowSum)) {
+				//this row is in trouble, find an open space!
+				for(var col = 0; col < pieceArray[row].length; col++){
+					if(pieceArray[row][col] === 0){
+						//Open!
+						displayO(row,col);
+						return;
+					}
+				}
+			}
+
+			//ltr or top to bottom diagonal, [0,0],[1,1][2,2]
+			if (advOrRisk(ltrDiagSum)) {
+				for(var col = 0; col < pieceArray[row].length; col++){
+					if(pieceArray[col][col] === 0){
+						//Open!
+						displayO(col,col);
+						return;
+					}
+				}
+			}
+
+			//rtl or bottom to top diagonal, [2,0],[1,1][0,2]
+			if (advOrRisk(rtlDiagSum)) {
+				for(var col = 0; col < pieceArray[row].length; col++){
+					if(pieceArray[pieceArray[row].length-col-1][col] === 0){
+						//Open!
+						displayO(pieceArray[row].length-col-1,col);
+						return;
+					}
+				}
+			}
+
+		}
+
+		//Just find the first open piece
+		for(var row = 0; row < pieceArray.length; row++){
+			for(var col = 0; col < pieceArray[row].length; col++){
+				if(pieceArray[row][col] === 0){
+					displayO(row,col);
+					return;
+				}
+			}
+		}
+
+	}
+
+	function advOrRisk(value){
+		if (value === O_ADV || value === X_RISK ) {
+			return true
+		}
+
+		return false;
+	}
+
+	//do some counting of place values that we have to do every turn
+	function countPieces(row){
+		rowSum = pieceArray[row].reduce(add, 0);
+		colSum = 0;
+
+		ltrDiagSum += pieceArray[row][row]; //00, 11, 22
+
+		//get the other diagonal
+		rRow = pieceArray.length - row - 1;
+		rtlDiagSum += pieceArray[rRow][row]; //20, 11, 02
+
+		//now check the nth (row) column
+		for(var col = 0; col < pieceArray[row].length; col++){
+			//reverse our cols and rows and we can check columns too!
+			colSum += pieceArray[col][row];
+		}
+	}
+
+	//Check all the rows, columns and diagonals
+	function checkWin(){
+		ltrDiagSum = 0;
+		rtlDiagSum = 0;
+
+		//are any of the rows in a win condition?
+		for(var row = 0; row < pieceArray.length; row++){
+
+			countPieces(row);
+
+			//Do we have a winner?
+			if(rowSum === X_WIN || colSum === X_WIN || ltrDiagSum === X_WIN || rtlDiagSum === X_WIN){
+				return 'X';
+			}else if (rowSum === O_WIN || colSum === O_WIN || ltrDiagSum === O_WIN || rtlDiagSum === O_WIN) {
+				return 'O';
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Reset the game
+	 */
+	function resetGame(){
+		pTurn = true;
+		currentTurn = 1;
+		pieceArray = [[0,0,0],[0,0,0],[0,0,0]];
+		WINNER = false;
+
+		var piece;
+
+		//reset our pieces
+		for(var row = 0; row < pieceArray.length; row++){
+			for(var col = 0; col < pieceArray[row].length; col++){
+
+				//build our piece
+				piece = PeaceMaker(pieceTemplate, {
+						row: row,
+						col: col
+					});
+
+				//put a blank svg in place
+				$('#square-'+row+'-'+col).html(piece);
+			}
+		}
+
+	}
+
+	//add 2 numbers.
 	function add(a, b) {
 		return a + b;
+	}
+
+	/**
+	 * Make pieces our of the given data object.
+	 */
+	function PeaceMaker(tpl, data) {
+		var re = /<%([^%>]+)?%>/g, match;
+		while(match = re.exec(tpl)) {
+			tpl = tpl.replace(match[0], data[match[1]])
+		}
+		return tpl;
 	}
 
 });
